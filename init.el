735ca05e5efffe7ed6ea5f1cffa9a75229e2922c
Taiyo <147013698+sodesudesu@users.noreply.github.com>
2023-11-15 23:02:25 +0900

    Add files via upload

---
+++ init.el
@@ -0,0 +1,160 @@
+;;
+;; YaTeX
+;;
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+(setq auto-mode-alist
+      (append '(("\\.tex$" . yatex-mode)
+                ("\\.ltx$" . yatex-mode)
+                ("\\.cls$" . yatex-mode)
+                ("\\.sty$" . yatex-mode)
+                ("\\.clo$" . yatex-mode)
+                ("\\.bbl$" . yatex-mode)) auto-mode-alist))
+(setq YaTeX-inhibit-prefix-letter t)
+(setq YaTeX-kanji-code nil)
+(setq YaTeX-latex-message-code 'utf-8)
+(setq YaTeX-use-LaTeX2e t)
+(setq YaTeX-use-AMS-LaTeX t)
+(setq YaTeX-dvi2-command-ext-alist
+      '(("TeXworks\\|texworks\\|texstudio\\|mupdf\\|SumatraPDF\\|Preview\\|Skim\\|TeXShop\\|evince\\|atril\\|xreader\\|okular\\|zathura\\|qpdfview\\|Firefox\\|firefox\\|chrome\\|chromium\\|MicrosoftEdge\\|microsoft-edge\\|Adobe\\|Acrobat\\|AcroRd32\\|acroread\\|pdfopen\\|xdg-open\\|open\\|start" . ".pdf")))
+;(setq tex-command "ptex2pdf -u -l -ot '-synctex=1'")
+(setq tex-command "lualatex -synctex=1") ;;;;;
+;(setq tex-command "latexmk")
+;(setq tex-command "latexmk -e '$latex=q/uplatex %O -synctex=1 %S/' -e '$bibtex=q/upbibtex %O %B/' -e '$biber=q/biber %O --bblencoding=utf8 -u -U --output_safechars %B/' -e '$makeindex=q/upmendex %O -o %D %S/' -e '$dvipdf=q/dvipdfmx %O -o %D %S/' -norc -gg -pdfdvi")
+;(setq tex-command "latexmk -e '$lualatex=q/lualatex %O -synctex=1 %S/' -e '$bibtex=q/upbibtex %O %B/' -e '$biber=q/biber %O --bblencoding=utf8 -u -U --output_safechars %B/' -e '$makeindex=q/upmendex %O -o %D %S/' -norc -gg -pdflua")
+;(setq bibtex-command "latexmk -e '$latex=q/uplatex %O -synctex=1 %S/' -e '$bibtex=q/upbibtex %O %B/' -e '$biber=q/biber %O --bblencoding=utf8 -u -U --output_safechars %B/' -e '$makeindex=q/upmendex %O -o %D %S/' -e '$dvipdf=q/dvipdfmx %O -o %D %S/' -norc -gg -pdfdvi")
+(setq bibtex-command "latexmk -e '$lualatex=q/lualatex %O -synctex=1 %S/' -e '$bibtex=q/upbibtex %O %B/' -e '$biber=q/biber %O --bblencoding=utf8 -u -U --output_safechars %B/' -e '$makeindex=q/upmendex %O -o %D %S/' -e '$dvipdf=q/dvipdfmx %O -o %D %S/' -norc -gg -pdfdvi") ;;;;;
+;(setq makeindex-command  "latexmk -e '$latex=q/uplatex %O -synctex=1 %S/' -e '$bibtex=q/upbibtex %O %B/' -e '$biber=q/biber %O --bblencoding=utf8 -u -U --output_safechars %B/' -e '$makeindex=q/upmendex %O -o %D %S/' -e '$dvipdf=q/dvipdfmx %O -o %D %S/' -norc -gg -pdfdvi")
+(setq makeindex-command "latexmk -e '$lualatex=q/lualatex %O -synctex=1 %S/' -e '$bibtex=q/upbibtex %O %B/' -e '$biber=q/biber %O --bblencoding=utf8 -u -U --output_safechars %B/' -e '$makeindex=q/upmendex %O -o %D %S/' -e '$dvipdf=q/dvipdfmx %O -o %D %S/' -norc -gg -pdfdvi");;;;;
+;(setq dvi2-command "xdg-open")
+(setq dvi2-command "evince")
+;(setq dvi2-command "atril")
+;(setq dvi2-command "okular --unique")
+;(setq dvi2-command "zathura -x \"emacsclient --no-wait +%{line} %{input}\"")
+;(setq dvi2-command "qpdfview --unique")
+;(setq dvi2-command "texworks")
+;(setq dvi2-command "texstudio --pdf-viewer-only")
+;(setq tex-pdfview-command "xdg-open")
+(setq tex-pdfview-command "evince")
+;(setq tex-pdfview-command "atril")
+;(setq tex-pdfview-command "okular --unique")
+;(setq tex-pdfview-command "zathura -x \"emacsclient --no-wait +%{line} %{input}\"")
+;(setq tex-pdfview-command "qpdfview --unique")
+;(setq tex-pdfview-command "texworks")
+;(setq tex-pdfview-command "texstudio --pdf-viewer-only")
+(setq dviprint-command-format "wine64 cmd /c start Acrobat.exe `echo %s | sed -e \"s/\\.[^.]*$/\\.pdf/\"`")
+(with-eval-after-load 'yatexprc
+  (defun YaTeX-preview-jump-line ()
+    "Call jump-line function of various previewer on current main file"
+    (interactive)
+    (save-excursion
+      (save-restriction
+        (widen)
+        (let*((pf (or YaTeX-parent-file
+                      (save-excursion (YaTeX-visit-main t) (buffer-file-name))))
+              (pdir (file-name-directory pf))
+              (bnr (substring pf 0 (string-match "\\....$" pf)))
+              ;(cf (file-relative-name (buffer-file-name) pdir))
+              (cf (buffer-file-name)) ;2016-01-08
+              (buffer (get-buffer-create " *preview-jump-line*"))
+              (line (count-lines (point-min) (point-end-of-line)))
+              (previewer (YaTeX-preview-default-previewer))
+              (cmd (cond
+                    ((string-match "Skim" previewer)
+                     (format "%s %d '%s.pdf' '%s'"
+                             YaTeX-cmd-displayline line bnr cf))
+                    ((string-match "evince" previewer)
+                     (format "%s '%s.pdf' %d '%s'"
+                             "fwdevince" bnr line cf))
+                    ((string-match "sumatra" previewer)
+                     (format "%s \"%s.pdf\" -forward-search \"%s\" %d"
+                             previewer bnr cf line))
+                    ((string-match "zathura" previewer)
+                     (format "%s --synctex-forward '%d:0:%s' '%s.pdf'"
+                             previewer line cf bnr))
+                    ((string-match "qpdfview" previewer)
+                     (format "%s '%s.pdf#src:%s:%d:0'"
+                             previewer bnr cf line))
+                    ((string-match "okular" previewer)
+                     (format "%s '%s.pdf#src:%d %s'"
+                             previewer bnr line (expand-file-name cf)))
+                    )))
+          (YaTeX-system cmd "jump-line" 'noask pdir))))))
+(add-hook 'yatex-mode-hook
+          '(lambda ()
+             (auto-fill-mode -1)))
+;;
+;; RefTeX with YaTeX
+;;
+;(add-hook 'yatex-mode-hook 'turn-on-reftex)
+(add-hook 'yatex-mode-hook
+          '(lambda ()
+             (reftex-mode 1)
+             (define-key reftex-mode-map (concat YaTeX-prefix ">") 'YaTeX-comment-region)
+             (define-key reftex-mode-map (concat YaTeX-prefix "<") 'YaTeX-uncomment-region)))
+
+;;; elpaの設定
+(require 'package)
+
+;; HTTP 系のリポジトリ
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+(package-initialize) ; インストール済みのElispを読み込む
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(helm rainbow-delimiters yatex markdown-preview-mode markdown-mode ess twittering-mode magit)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;; markdown mode
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+;;; markdown preview mode
+(setq markdown-preview-stylesheets 
+      (list "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css"))
+
+;;; auto-complete（補完）の設定
+(when (require 'auto-complete-config nil t)
+  (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+  (ac-config-default)
+  (setq ac-use-menu-map t)
+  (setq ac-ignore-case nil)
+  (global-auto-complete-mode t)
+  (add-hook 'yatex-mode-hook 'auto-complete-mode)
+  ;; YaTeX用の補完辞書を追加
+  (defun my-yatex-ac-setup ()
+    (setq ac-sources
+          (append '(ac-source-yatex) ac-sources)))
+  (add-hook 'yatex-mode-hook 'my-yatex-ac-setup))
+
+;;; 行番号の表示
+(require 'linum)
+(global-linum-mode t)
+(setq linum-format "%4d")
+
+;;; python
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "--matplotlib --no-automagic -i")
+
+;;; ESSの設定
+;; コメント行が勝手に中央寄せになるのを解消
+(setq ess-indent-with-fancy-comments nil)
+
+;;; 括弧の設定
+;; あらかじめrainbow-delimitersをインストールしておく
+(require 'rainbow-delimiters) ; 括弧の色付け
+(global-rainbow-delimiters-mode)
+
+;;; helm
+(require 'helm-config)
